8e254a35b161704e9757968ce2d3336a50a0e005
Natan <contato.natanpereira@gmail.com>
2025-07-21 10:06:35 -0300

    Script_Estrutura_Projeto_PHP

---
+++ script.php
@@ -0,0 +1,133 @@
+<?php 
+// FUNÇÃO PARA CRIAR DIRETÓRIOS
+function create_directory($directory){
+    if(!is_dir($directory)){
+        if(mkdir($directory, 0777, true)){
+            echo "Diretório '$directory' criado com sucesso!" . PHP_EOL;
+        }else{
+            echo "Erro ao tentar criar o diretório '$directory'" . PHP_EOL;
+        }
+    }else{ 
+        echo 'Diretório já está criado.' . PHP_EOL;
+    }
+}
+
+$paths = array(
+        'src',
+        'src/assets',
+        'src/assets/css',
+        'src/assets/img',
+        'src/data',
+        'src/pages',
+        'src/scripts'
+    );
+
+foreach($paths as $path){
+    create_directory($path);
+}
+
+//ARQUIVO QUE SERÃO CRIADOS NOS DIRETÓRIOS
+$cssContent = 
+<<<CSS
+*{ 
+margin:0;
+padding:0; 
+box-sizing:border-box;
+}
+CSS;
+$dbConfigContent = 
+<<<DBCONFIG
+<?php 
+\$db_host='';
+\$db_name='';
+\$db_user='';
+\$db_passwd='';
+
+if(\$_SERVER['REQUEST_METHOD'] === 'POST'){
+    try{
+        \$pdo = new PDO("mysqli:host=\$db_host;dbname=\$db_name;charset=utf8", \$db_user, \$db_passwd);
+        \$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch (PDOException \$erro){
+        echo "Erro ao tentar conectar com o banco de dados" . \$erro->getMessage();
+    }
+}
+?>
+DBCONFIG;
+$files = array(
+ 'src/assets/css/Index.css' => "$cssContent",
+ 'src/data/DB_Config.php' => "$dbConfigContent"
+);
+
+foreach($files as $file => $content){
+    if(!file_exists($file)){
+        if(file_put_contents($file, $content) !== false){
+            echo "Arquivo '$file' criado com sucesso!" . PHP_EOL;
+        }else{
+            echo "Erro ao tentar criar '$file'" . PHP_EOL;
+        }
+    }else{
+        echo "Arquivo {$file} já foi criado." . PHP_EOL;
+    }        
+    
+}
+
+//ARQUIVOS QUE SERÃO CRIADOS NA RAIZ DO PROJETO
+$indexContent = 
+<<<HTML
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- LINK CSS  -->
+    <link rel="stylesheet" href="./src/assets/css/index.css"> 
+    </head>
+    <body>
+            
+    </body>
+</html>
+HTML;
+
+$envContent = 
+<<<ENV
+#CONFIG DATABASE
+DB_HOST=
+DB_NAME=
+DB_USER=
+DB_PASSWD=
+DB_PORT=
+
+#SECRET KEY
+API_KEY=
+JWT_KEY=
+ENV;
+
+$gitignoreContent = 
+<<<GITIGNORE
+/node_modules/
+/vendor/
+.env
+*.log
+*.tmp
+*.cache
+GITIGNORE;
+$files = array(
+    'index.php' => "$indexContent",
+    '.env' => "$envContent",
+    '.gitignore' => "$gitignoreContent",
+
+);
+
+foreach($files as $file => $content){
+    if(!file_exists($file)){
+        if(file_put_contents($file, $content) !== false){
+            echo "Arquivo '$file' criado." . PHP_EOL;
+        }else{
+            echo "Erro ao tentar criar o arquivo '$file' " . PHP_EOL;
+        }
+    }else{
+        echo "Arquivo '$file' já está criado!" . PHP_EOL;
+    }
+}
+        
